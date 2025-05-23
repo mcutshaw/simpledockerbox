f7d0aaec985c881ae8e47cb9bfe29773451f5227
mcutshaw <mike.cutshaw@hotmail.com>
2025-05-22 21:49:17 -0600

    fixes

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-docker build -t simpleboxbase:latest . -f base/Dockerfile 
-docker build -t simpleboxopenbox:latest . -f openbox/Dockerfile #  && docker run -it -p 127.0.0.1:6080:6080 simpleboxopenbox:latest
-docker build -t simpleboxlxqt:latest . -f lxqt/Dockerfile  && docker run -it -p 127.0.0.1:6080:6080 simpleboxlxqt:latest
+docker build -t simpledockerbox:base . -f base/Dockerfile 
+docker build -t simpledockerbox:openbox . -f openbox/Dockerfile #  && docker run -it -p 127.0.0.1:6080:6080 simpledockerbox:openbox
+docker build -t simpledockerbox:lxqt . -f lxqt/Dockerfile  && docker run -it -p 127.0.0.1:6080:6080 simpledockerbox:lxqt
